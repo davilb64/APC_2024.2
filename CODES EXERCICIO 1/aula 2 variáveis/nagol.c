f21dc8e1eaa5568c677a5b2496d30435696cd19a
Davi Lopes Brito <162513480+davilb64@users.noreply.github.com>
2024-11-22 15:47:52 -0300

    Update nagol.c

--- CODES EXERCICIO 1/aula 2 variáveis/nagol.c
+++ CODES EXERCICIO 1/aula 2 variáveis/nagol.c
@@ -2,8 +2,29 @@
  
 int main() {
  
-    int l,c,x,y;
+    int l,c,x,y,sobrax,sobray;
     scanf("%d%d%d%d",&l,&c,&x,&y);
+    sobrax = x%2;
+    sobray = y%2;
+    if (sobrax == 0)
+    {
+        if (sobray == 0)
+        {
+            printf("Direita");
+        }
+        
+    }
+    else if (sobrax =! 0)
+    {
+        if (sobray == 0)
+        {
+            
+        }
+        
+    }
+    {
+
+    }
     
     
     return 0;
